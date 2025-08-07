b40d571b03505f816d6c1f581db444937cd6989b
Juneja16 <rjhk6767@gmail.com>
2025-08-07 23:40:16 +0530

    Soda Vending Machine Code

---
+++ 3_Ethereum_SmartContracts/SodaVendingMachine.sol
@@ -0,0 +1,30 @@
+program solidity ^0.8.20;
+
+// A vending Machine Automated code to purchase the soda
+constract VendingMachine{
+    uint soda;
+    address owner;
+
+    constructor(){
+        soda=100;
+        owner=msg.sender;
+    }
+
+    function buySoda() public payable{
+        require(msg.value == 1 ether, "You should have one ether");
+        require(soda>0,"Soda Out of Stock"); 
+         soda=soda-1;
+    }
+
+    function fillSoda(_quantity) public (){
+        require(msg.sender== owner,"You are not the Owner");
+        soda =soda +_quantity;
+
+    }
+
+    function withdrawBalance() public{
+        require(msg.sender == owner,"You are not the Owner");
+        payable (owner).transfer(address(this).balance);
+
+    }
+}
